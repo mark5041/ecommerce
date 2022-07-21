49ec0cb22b9c4faad7682828fd22981d39cf4110
Betacom <Betacom@Betacom-PC5039>
2022-07-21 12:17:58 +0200

    ordineservice

---
+++ src/main/java/com/betacom/businesscomponent/service/OrdineService.java
@@ -0,0 +1,52 @@
+package com.betacom.businesscomponent.service;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+
+import javax.naming.NamingException;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.betacom.businesscomponent.OrdineBC;
+import com.betacom.businesscomponent.model.Ordine;
+
+@Path("/ordineservice")
+public class OrdineService {
+
+	@GET
+	@Path("/ordini")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Ordine[] getOrdini() throws NamingException, SQLException {
+		OrdineBC oBC = new OrdineBC();
+		return oBC.getOrdini();
+	}
+	
+	@POST
+	@Path("/createordine")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Transactional
+	public Ordine createOrdine(Ordine ordine) 
+			throws NamingException, SQLException, ParseException {
+		OrdineBC oBC = new OrdineBC();
+		oBC.create(ordine);
+		return ordine;
+	}
+	
+	@DELETE
+	@Path("/deleteordine/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Transactional
+	public void deleteOrdine(@PathParam("id") long id) 
+			throws NamingException, SQLException {
+		OrdineBC oBC = new OrdineBC();
+		oBC.delete(id);
+	}
+}
