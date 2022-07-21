df087624108526b8ec817569aa04854beba83d8d
Betacom <Betacom@Betacom-PCCE51.homenet.telecomitalia.it>
2022-07-21 12:51:47 +0200

    insert.sql

--- SQL/insert.sql
+++ SQL/insert.sql
@@ -6,3 +6,24 @@ insert into cliente(nome, cognome, username, password, email) values
 ('Soldier', 'Boy', 'SoldierBoy', 'pass', 'SoldierBoy@gmail.com');
 
 commit
+
+insert into prodotto(marca, modello, prezzo) values
+("Apple", "iPhone 13", "1200");
+insert into prodotto(marca, modello, prezzo) values
+("Samsung", "S20", "800");
+insert into prodotto(marca, modello, prezzo) values
+("ASUS", "VivoBook Pro 17", "1400");
+insert into prodotto(marca, modello, prezzo) values
+("Apple", "MacBook Pro", "2000");
+insert into prodotto(marca, modello, prezzo) values
+("Apple", "iPad", "1000");
+
+commit
+
+insert into ordine(data, indirizzo, totale, id_cliente) values
+(CURDATE(), 'via Roma, 23', 2000, 1);
+insert into ordine(data, indirizzo, totale, id_cliente) values
+(CURDATE(), 'via Milano, 69', 3000, 2);
+
+commit
+
